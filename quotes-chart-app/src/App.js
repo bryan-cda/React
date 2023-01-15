7a445376e8c7f589f7ba4746449f92497725cfc2
root <root@ILEGRA-DDTRCJ3.localdomain>
2023-01-15 11:51:36 -0300

    :hammer: create a file canvasjs.min.js.#

--- quotes-chart-app/src/App.js
+++ quotes-chart-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Bruce's App <code>edit src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
